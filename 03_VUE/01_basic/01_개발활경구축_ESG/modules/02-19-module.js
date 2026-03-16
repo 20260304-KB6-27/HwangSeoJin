5bc315525428131f30fc31c8cf168b7202537fd4
HwangSeo <sadclara0830@naver.com>
2026-03-16 16:43:05 +0900

    개발환경구축 실습 제출

---
+++ 03_VUE/01_basic/01_개발활경구축_ESG/modules/02-19-module.js
@@ -0,0 +1,9 @@
+let base = 100;
+// const add = (x) => base+x
+// const multiply = (x) => base*x;
+// export { add, multiply };
+// export default getBase;
+
+export const add = (x) => base+x
+export const multiply = (x) => base*x;
+export default () => base;
