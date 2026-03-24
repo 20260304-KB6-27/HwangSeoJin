7efce8af9a474fc29f49f83b9aac6398a11303e0
HwangSeo <sadclara0830@naver.com>
2026-03-24 15:42:15 +0900

    단일파일컴포넌트 실습 저장

---
+++ 03_VUE/01_basic/06_단일파일컴포넌트/test-vue-app/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+//import App from './App.vue'
+//import App from './App1.vue' 
+//import App from './App2.vue' 
+//import App from './App3.vue' 
+import App from './App4.vue' 
+
+// 전역설정
+//import Checkboxitem from './components/Checkboxitem.vue';
+//createApp(App).component("CheckboxItem", Checkboxitem).mount('#app')
+createApp(App).mount('#app')
---
+++ 03_VUE/02_advanced/06_단일파일컴포넌트/99_example_v3/src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>단일파일컴포넌트로 게시판 바꾸기 </h1>
+  
+</template>
+
+<style scoped></style>
