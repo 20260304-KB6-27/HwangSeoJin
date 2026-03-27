cc4bffbb4b68b8ed3336cd06126a1f80e6e89d86
HwangSeo <sadclara0830@naver.com>
2026-03-27 15:06:13 +0900

    Composition API 실습 제출

---
+++ 03_VUE/01_basic/08_Composition/cals-component-test/src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+    <h1>Carc1</h1>
+    <Calc/>
+    <hr>
+    <h1>Carc2</h1>
+    <Calc2/>
+    <hr>
+    <h1>Carc3</h1>
+    <Calc3/>
+    <hr>
+    <h1>Carc4</h1>
+    <Calc4/>
+    <hr>
+    <h1>Carc5</h1>
+    <Calc5/>
+  </div>
+
+</template>
+
+<script setup>
+import Calc from './components/Calc.vue';
+import Calc2 from './components/Calc2.vue';
+import Calc3 from './components/Calc3.vue';
+import Calc4 from './components/Calc4.vue';
+import Calc5 from './components/Calc5.vue';
+
+</script>
+
+<style scoped></style>
