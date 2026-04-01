2fb30c1e4001544792a0cd8f90cb3a31002a1bfb
HwangSeo <sadclara0830@naver.com>
2026-04-01 16:06:48 +0900

    라우터 실습 제출

---
+++ 03_VUE/02_advanced/09_Router/router-test/src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청 경로 : {{ route.fullPath }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+console.log('라우터 객체:', router)
+</script>
